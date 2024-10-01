9e9e6e1856678b706e2c4e9c7ecf7258129f47f6
PreciousEzeigbo <preciousezeigbo81@gmail.com>
2024-10-01 14:24:21 +0100

    Task 7

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,9 @@
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income, // Shorthand for income: income
+    gdp, // Shorthand for gdp: gdp
+    capita, // Shorthand for capita: capita
+  };
+
+  return budget;
+}
