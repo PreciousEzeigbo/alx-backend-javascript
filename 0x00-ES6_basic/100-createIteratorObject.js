860e4f3f04599e0b11e11fadd36615dcc2521f95
PreciousEzeigbo <preciousezeigbo81@gmail.com>
2024-10-01 14:59:23 +0100

    Advanced task

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,27 +1,13 @@
 export default function createIteratorObject(report) {
-  const departments = Object.keys(report.allEmployees);
-  let departmentIndex = 0;
-  let employeeIndex = 0;
-
-  return {
-    next() {
-      if (departmentIndex >= departments.length) {
-        return { done: true };
+  function* employeeIterator() {
+    for (const department in report.allEmployees) {
+      // Use Object.prototype.hasOwnProperty to check for the property
+      if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
+        for (const employee of report.allEmployees[department]) {
+          yield employee; // Yield each employee
+        }
       }
-
-      const department = departments[departmentIndex];
-      const employees = report.allEmployees[department];
-
-      if (employeeIndex >= employees.length) {
-        departmentIndex += 1;
-        employeeIndex = 0;
-        return this.next();
-      }
-
-      const employee = employees[employeeIndex];
-      employeeIndex += 1;
-
-      return { value: employee, done: false };
-    },
-  };
+    }
+  }
+  return employeeIterator(); // Return the generator
 }
