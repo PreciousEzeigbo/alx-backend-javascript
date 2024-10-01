8490847656ac9ec02819811dc221571df11edfad
PreciousEzeigbo <preciousezeigbo81@gmail.com>
2024-10-01 14:06:26 +0100

    Advanced Task

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,13 +1,7 @@
 export default function createIteratorObject(report) {
-  const departments = Object.values(report.allEmployees);
-
-  return {
-    * [Symbol.iterator]() {
-      for (const department of departments) {
-        for (const employee of department) {
-          yield employee;
-        }
-      }
-    },
-  };
+    const employees = [];
+    for (const department of Object.values(report.allEmployees)) {
+      employees.push(...department);
+    }
+    return employees[Symbol.iterator]();
 }
